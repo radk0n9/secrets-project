64e7b1026886cc1727844e9f3cee575bb04a4a6f
Konrad Pawelec <konrad.pawelec@icloud.com>
2023-03-28 19:45:29 +0200

    Added Register and Login with Passport

--- app.js
+++ app.js
@@ -4,29 +4,50 @@ const bodyParser = require("body-parser");
 const _ = require("lodash");
 const express = require("express");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
-// const md5 = require("md5");
-// const encrypt = require("mongoose-encryption");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const LocalStategy = require("passport-local").Strategy;
 
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use(session({
+    secret: "djsald190djsakldj190djsk@",
+    resave: false,
+    saveUninitialized: true
+}));
 
-mongoose.connect("mongodb://localhost:27017/userDB");
+// Configutre passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
 
-// userSchema.plugin(encrypt, {secret: process.env.SECRET_KEY, encryptedFields: ["password"]});
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
 
+passport.use(new LocalStategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Connect to mongoose DB
+mongoose.connect("mongodb://localhost:27017/userDB").catch((err)=>{
+    if (err){
+        console.log("Could not connect to mongodb on localhost.");
+    };
+});
+
+// Routes for all ejs
 app.route("/")
     .get(function(req, res){
         res.render("home");
@@ -35,56 +56,37 @@ app.route("/")
         console.log(req.params);
     });
 
+app.route("/secrects")
+    .get(function(req, res){
+        if (req.isAuthenticated()){
+            res.render("secrects");
+        }else{
+            res.redirect("login");
+        }
+    });
+
 app.route("/register")
     .get(function(req, res){
         res.render("register")
     })
     .post(function(req, res){
-        bcrypt.hash(req.body.password, saltRounds, function(err, hash){
-            const newUser = new User({
-                email: req.body.username,
-                password: hash
-            });
-            newUser.save().then((message)=>{
-                if (message){
-                    res.render("secrects");
-                };
-            }).catch((error)=>{
-                if (error){
-                    console.log(error);
-                };
-            });
-        })  
+        User.register(new User({username: req.body.username}), req.body.password, function(err){
+            if (err){
+                console.log("Error while user register!", err);
+                res.redirect("register");
+            }else{
+                console.log("User registered");
+                res.redirect("/secrects");
+            };
+        }); 
     });
 
 app.route("/login")
     .get(function(req, res){
         res.render("login");
     })
-    .post(function(req, res){
-        const username = req.body.username;
-        const password = req.body.password;
-
-        User.findOne({email: username}).then((foundUser)=>{
-            if (foundUser){
-                bcrypt.compare(password, foundUser.password, function(err, result){
-                    if (result){
-                        res.render("secrects");
-                    }else{
-                        res.redirect("login");
-                    };
-                })                    
-            };
-        }).catch((error)=>{
-            if (error){
-                console.log(error);
-            };
-        });
-    });
-
-app.route("/secrects")
-    .get(function(req, res){
-        res.render("secrects")
+    .post(passport.authenticate("local", {failureRedirect: "/login", failureFlash: true}), function(req, res){
+        res.redirect("/secrects");
     });
 
 app.listen(port, function(){
