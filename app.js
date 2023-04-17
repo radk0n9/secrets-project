57f30510c0b1e0f693c9d41375f172fd701b0df1
Konrad Pawelec <konrad.pawelec@icloud.com>
2023-04-17 18:38:15 +0200

    User can submit a Secret

--- app.js
+++ app.js
@@ -32,7 +32,8 @@ app.use(passport.session());
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -99,11 +100,42 @@ app.get("/auth/google/secrets",
 
 app.route("/secrets")
     .get(function(req, res){
+        User.find({"secret":{$ne: null}}).then((foundUsers)=>{
+            if(foundUsers){
+                res.render("secrets", {usersWithSecrets: foundUsers})
+            };
+        }).catch((error)=>{
+            if (error){
+                console.log(error);
+            };
+        });
+    });
+
+app.route("/submit")
+    .get(function(req, res){
         if (req.isAuthenticated()){
-            res.render("secrets");
+            res.render("submit");
         }else{
             res.redirect("login");
         }
+    })
+    .post(function(req, res){
+        const submittedSecret = req.body.secret;
+
+        User.findById(req.user.id).then((foundUser)=>{
+            if(foundUser){
+                foundUser.secret = submittedSecret;
+                foundUser.save().then(function(){
+                    res.redirect("secrets");
+                });
+            }else{
+                console.log(foundUser);
+            };
+        }).catch((error)=>{
+            if(error){
+                console.log(error)
+            };
+        });
     });
 
 app.route("/register")
